76ecf82aa8c010073387c8b39dd3e20805957a2b
prachijustin <prachi.sharma01sep@gmail.com>
2020-08-26 11:48:23 +0530

    action types added

---
+++ src/Redux/Actions/ActionTypes.js
@@ -0,0 +1,7 @@
+export const CREAT_POST = "CREAT_POST";
+export const EDIT_POTS = "EDIT_POTS";
+export const REMOVE_POST = "REMOVE_POST";
+export const POSTS_LIST = "POSTS_LIST";
+export const LIKE_POST = "LIKE_POST";
+export const SEARCH_POST = "SEARCH_POST";
+export const VIEW_POST = "VIEW_POST";
